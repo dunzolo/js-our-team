4ae462522f813136ae5c1aaf0c919ac68e2e78e1
dunzolo <rossidavide108@gmail.com>
2022-12-13 14:46:23 +0100

    milestone 0 done: added array

---
+++ js/script.js
@@ -0,0 +1,35 @@
+//creo l'array di oggetti rappresentante i membri del team
+let teams = [
+    {
+        name: "Wayne Barnet",
+        role: "Founder & CEO",
+        image: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        image: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        image: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopex",
+        role: "Social Media Manager",
+        image: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        image: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        image: "barbara-ramos-graphic-designer.jpg"
+    }
+]
+
+console.log(teams);
